7a0ffb1101cd3efabd824f460c1db13f81b1f9d2
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-17 17:54:10 +0200

    Métodos crear y dibujar Tablero

---
+++ ProjBuscaminas/js/buscaminas_obj.js
@@ -0,0 +1,43 @@
+class Tablero {
+    constructor(filas, columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.crearTablero();
+        this.dibujarTablero();
+    }
+
+    crearTablero() {
+
+        // Crear array bidimensional para guardar las minas
+        this.arrayTablero = [];
+
+        for (let fila = 0; fila < this.filas; fila++) {
+            this.arrayTablero[fila] = new Array(this.columnas);
+
+            for (let columna = 0; columna < this.columnas; columna++) {
+                this.arrayTablero[fila][columna] = '';
+            }
+        }
+
+    }
+
+    dibujarTablero() {
+
+        // Creamos el tablero en html
+        document.write('<table>');
+
+        for (let i = 0; i < this.filas; i++) {
+            document.write('<tr>');
+
+            for (let j = 0; j < this.columnas; j++) {
+                document.write('<td>' + this.arrayTablero[i][j] + '</td>');
+            }
+
+            document.write('</tr>');
+        }
+        document.write('</table>');
+    }
+}
+
+const buscaminas = new Tablero(3, 4);
+console.log(buscaminas.arrayTablero);
