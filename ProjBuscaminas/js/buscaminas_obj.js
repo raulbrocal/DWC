01e6a91c17f73bd9849ee25bcbd74c469f5969d4
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-18 18:34:10 +0200

    Añadido modificarColumnas y modificarFilas

--- ProjBuscaminas/js/buscaminas_obj.js
+++ ProjBuscaminas/js/buscaminas_obj.js
@@ -37,6 +37,22 @@ class Tablero {
         }
         document.write('</table>');
     }
+
+    modificarFilas(nuevasFilas){
+    // Modificar el número de filas y volver a crear el tablero con las filas nuevas.
+
+        this.filas = nuevasFilas;
+        this.crearTablero();
+
+    }
+
+    modificarColumnas(nuevasColumnas) {
+    // Modificar el número de columnas y volver a crear el tablero con las columnas nuevas.
+
+        this.columnas = nuevasColumnas;
+        this.crearTablero();
+        
+    }
 }
 
 const buscaminas = new Tablero(4, 4);
