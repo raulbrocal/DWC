c8aea65866af9727a1b87beb6993dbd2ebbdb81e
raulbrocal <raulbrocalberral16@gmail.com>
2022-10-24 17:55:06 +0200

    Implementar imgs y clase JuegoMemoria

--- ProyJuegoMemoria/js/juegoMemoria.js
+++ ProyJuegoMemoria/js/juegoMemoria.js
@@ -10,8 +10,8 @@ class Tablero {
     numFilasColumnas() {
 
         // Se piden los datos al usuario
-        let filas = prompt('¿Cuántas filas quieres?');
-        let columnas = prompt('¿Cuántas columnas quieres?');
+        let filas = 4;//prompt('¿Cuántas filas quieres?');
+        let columnas = 4;//prompt('¿Cuántas columnas quieres?');
 
         while (filas * columnas % 2 != 0 || filas < 2 || columnas < 2) {
             alert("No es posible tener filas y columnas impares o menores que 2.");
@@ -32,7 +32,7 @@ class Tablero {
             this.arrayTablero[fila] = new Array(this.columnas);
 
             for (let columna = 0; columna < this.columnas; columna++) {
-                this.arrayTablero[fila][columna] = '';
+                this.arrayTablero[fila][columna] = '.';
             }
         }
 
@@ -71,6 +71,36 @@ class Tablero {
 
     }
 
+
+
+}
+
+class JuegoMemoria extends Tablero {
+    constructor(filas, columnas) {
+        super(filas, columnas);
+
+        this.colocarParejas()
+    }
+
+    colocarParejas() {
+        let parejas = ["./imgs/corazonAmarillo.png", "./imgs/corazonAzul.png", "./imgs/corazonAmarillo.png", "./imgs/corazonBlanco.png", "./imgs/corazonMarron.png"];
+        let posFila;
+        let posColumna;
+        let numParejas = 0;
+
+
+        while (numParejas < 4) {
+            posFila = Math.floor(Math.random() * this.filas);
+            posColumna = Math.floor(Math.random() * this.columnas);
+
+            if (this.arrayTablero[posFila][posColumna] = '.') {
+                this.arrayTablero[posFila][posColumna] = parejas[0];
+                numParejas++;
+            };
+        };
+
+        return this.arrayTablero
+    }
 }
 
 let juegoMemoria = new Tablero();
