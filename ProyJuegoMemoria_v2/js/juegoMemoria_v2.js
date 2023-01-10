2c06963ed705dca1fff0c967874af1795e25b4b6
raulbrocal <raulbrocalberral16@gmail.com>
2023-01-10 18:52:24 +0100

    Añadir clase TableroMemorin -> intro. parejas des.

--- ProyJuegoMemoria_v2/js/juegoMemoria_v2.js
+++ ProyJuegoMemoria_v2/js/juegoMemoria_v2.js
@@ -83,6 +83,67 @@ class Tablero {
 
 }
 
+class TableroMemorin extends Tablero {
+    constructor(numFilas, numColumnas) {
+        super(numFilas, numColumnas);
+        this.numCasillas = this.numFilas * this.numColumnas;
+
+        this.crearArrayImagenes(`imagenes/`);
+
+        let numImgRepes = 2;
+
+        let arrayParejas = this.crearArrayImgRepes(numImgRepes);
+        let arrayParejasDesordenadas = this.desordenarArray(arrayParejas);
+        this.llenarTablero(arrayParejasDesordenadas);
+    }
+
+    crearArrayImagenes(url) {
+        this.imagenes = [];
+        let maxImagenes = 10;
+
+        for (let numImagen = 1; numImagen <= maxImagenes; numImagen++) {
+            this.imagenes.push(`${url}img${numImagen}.jpg`);
+        }
+    }
+
+    crearArrayImgRepes(numImgRepes) {
+        let arrayImgRepes = [];
+        let pos = 0;
+        let contCasillas = 0;
+
+        while (contCasillas < this.numCasillas) {
+            for (let numImg = 0; numImg < numImgRepes; numImg++) {
+                arrayImgRepes.push(this.imagenes[pos]);
+                contCasillas++;
+            }
+
+            pos++;
+
+            if (pos == this.imagenes.length) {
+                pos = 0;
+            }
+        }
+
+        return arrayImgRepes;
+    }
+
+    desordenarArray(array) {
+        return array.sort(() => 0.5 - Math.random());
+    }
+
+    llenarTablero(arrayImgRepes) {
+        let contador = 0;
+
+        for (let fila = 0; fila < this.numFilas; fila++) {
+            for (let columna = 0; columna < this.numColumnas; columna++) {
+                this.tablero[fila][columna] = `<img src="${arrayImgRepes[contador]}">`;
+                contador++;
+            }
+        }
+    }
+
+}
+
 window.onload = function () {
     // Iniciamos el juego.
     let juegoMemoria = new Tablero();
