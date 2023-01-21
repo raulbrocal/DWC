11fcd355a115efe41781135f7a02e5bd8001204e
raulbrocal <raulbrocalberral16@gmail.com>
2023-01-21 12:02:05 +0100

    Propuesta solución juego (puntuación incorrecta)

--- ProyJuegoMemoria_v2/js/juegoMemoria_v2.js
+++ ProyJuegoMemoria_v2/js/juegoMemoria_v2.js
@@ -6,25 +6,19 @@ document.write(`<b>Puntuación: <var id='puntuacion'>0</var>/<var id='maxPuntuac
 document.write('<br><br>');
 document.write('<input name="reiniciar" type="button" onclick="if (confirm(`Deseas reiniciar la partida ?`) == true) {location.reload();}" value="Reiniciar"/>')
 document.write('<div id="tiempo">00:00:00.000</div>');
-
 class Tablero {
-
     constructor() {
         // Datos necesarios para crear el tablero.
         this.numFilasColumnas();
         this.crearTableroVacio();
         this.maxPuntuacion();
     }
-
     numFilasColumnas() {
-
         // Se piden los datos al usuario
         let filas = prompt('¿Cuántas filas quieres?');
         let columnas = prompt('¿Cuántas columnas quieres?');
-
         // Casos en los que no se puede crear el tablero.
         while (filas * columnas % 2 != 0 || filas < 2 || columnas < 2 || filas > 100 || columnas > 100) {
-
             switch (filas * columnas % 2 != 0 || filas < 2 || columnas < 2 || filas * columnas > 100) {
                 case filas < 2 || columnas < 2:
                     alert("No es posible tener un número de filas o columnas menores que 2.");
@@ -39,39 +33,29 @@ class Tablero {
                     alert("No es posible generar parejas al tener ambos números impares.");
                     break;
             }
-
             filas = prompt('¿Cuántas filas quieres?');
             columnas = prompt('¿Cuántas columnas quieres?');
-
         }
-
         return this.filas = filas, this.columnas = columnas;
-
     }
-
     crearTableroVacio() {
         // Crear array bidimensional para guardar las parejas
         this.arrayTablero = [];
-
         for (let fila = 0; fila < this.filas; fila++) {
             this.arrayTablero[fila] = [];
-
             for (let columna = 0; columna < this.columnas; columna++) {
                 this.arrayTablero[fila][columna] = '';
             }
         }
     }
-
     dibujarTableroDOM() {
         // Creamos el tablero en DOM
         let tabla = document.createElement('table');
         let fila;
         let columna;
-
         for (let i = 0; i < this.filas; i++) {
             fila = document.createElement('tr');
             tabla.appendChild(fila);
-
             for (let j = 0; j < this.columnas; j++) {
                 columna = document.createElement('td');
                 columna.id = `f${i}_c${j}`;
@@ -80,20 +64,16 @@ class Tablero {
                 fila.appendChild(columna);
             }
         }
-
         document.body.appendChild(tabla);
     }
-
     maxPuntuacion() {
         let maxPuntuacion = ((this.filas * this.columnas) * 10) / 2;
         let nodoMaxPuntuacion = document.getElementById("maxPuntuacion");
         nodoMaxPuntuacion.innerHTML = maxPuntuacion;
     }
-
 }
-
 class JuegoMemoria extends Tablero {
-    constructor(filas, columnas, _puntuacion) {
+    constructor(filas, columnas) {
         super(filas, columnas);
         this.puntuacion = 0;
         this.numParejas = 0;
@@ -102,11 +82,9 @@ class JuegoMemoria extends Tablero {
         this.dibujarTableroDOM();
         this.cronometro();
     }
-
     colocarParejas() {
         // Array de emojis que formaran las parejas
         let parejas = ['&#10084;&#65039', '&#129505', '&#10084;&#65039;&#8205;&#128293', '&#128155', '&#128154', '&#128153', '&#128156', '&#129294', '&#128420', '&#129293'];
-
         // Colocamos de forma aleatoria las parejas necesarias.
         let contadorParejas = 0;
         let parejaRealizada = 0;
@@ -114,14 +92,10 @@ class JuegoMemoria extends Tablero {
         let posColumna;
         let contador = 0;
         let maxParejas = this.filas * this.columnas;
-
         while (contadorParejas < (this.filas * this.columnas)) {
-
             do {
-
                 posFila = Math.floor(Math.random() * this.filas);
                 posColumna = Math.floor(Math.random() * this.columnas);
-
                 if (!this.arrayTablero[posFila][posColumna].startsWith('&')) {
                     this.arrayTablero[posFila][posColumna] = parejas[contador];
                     contadorParejas++;
@@ -130,31 +104,21 @@ class JuegoMemoria extends Tablero {
                         break;
                     };
                 };
-
-
             } while (parejaRealizada != 2 || contadorParejas == 1);
-
             console.log(parejas[contador]);
             contador++;
             parejaRealizada = 0;
-
             if (contador == parejas.length) {
                 contador = 0;
             }
-
         };
-
         console.log(this.arrayTablero);
-
         return this.arrayTablero
     }
-
     dibujarTableroDOM() {
         super.dibujarTableroDOM();
         let celda;
-
         this.despejar = this.despejar.bind(this);
-
         for (let i = 0; i < this.filas; i++) {
             for (let j = 0; j < this.columnas; j++) {
                 celda = document.getElementById(`f${i}_c${j}`);
@@ -162,136 +126,76 @@ class JuegoMemoria extends Tablero {
             }
         }
     }
-
     despejar(elEvento) {
         let evento = elEvento || window.event;
         let celda = evento.currentTarget;
-
         let fila = parseInt(celda.dataset.fila);
         let columna = parseInt(celda.dataset.columna);
-
         // Marcar la celda despejada
         celda.style.backgroundColor = "lightgrey";
-
         let valorCelda = this.arrayTablero[fila][columna];
-
         if (this.primerEmoji === undefined) {
             celda.innerHTML = valorCelda;
             this.celda1 = celda;
             this.celda1.removeEventListener('click', this.despejar);
             this.primerEmoji = valorCelda;
-
             if (this.celdaReferencia != this.celda1) {
                 this.celdaReferencia = this.celda1;
                 this.intentos = 1;
             } else {
                 this.intentos++;
             }
-
         } else {
-
             celda.innerHTML = valorCelda;
             this.celda2 = celda;
             this.segundoEmoji = valorCelda;
             this.celda1.addEventListener('click', this.despejar);
-
             if (this.primerEmoji == this.segundoEmoji && this.celdaReferencia == this.celda1) {
-
+                this.numParejas += 1;
                 this.primerEmoji = undefined;
                 this.segundoEmoji = undefined;
                 this.celda1.removeEventListener('click', this.despejar);
                 this.celda2.removeEventListener('click', this.despejar);
-
                 let nodoPuntuacion = document.getElementById("puntuacion");
-
                 switch (this.intentos) {
                     case this.intentos = 1:
                         nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 10;
-                        setTimeout(this.finalizar.bind(this), 10);
+                        setTimeout(this.finalizar.bind(this),10);
                         break;
                     case this.intentos = 2:
                         nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 5;
-                        setTimeout(this.finalizar.bind(this), 10);
+                        setTimeout(this.finalizar.bind(this),10);
                         break;
                     case this.intentos = 3:
                         nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 2.5;
-                        setTimeout(this.finalizar.bind(this), 10);
+                        setTimeout(this.finalizar.bind(this),10);
                         break;
                     case this.intentos > 3:
                         nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 0;
-                        setTimeout(this.finalizar.bind(this), 10);
+                        setTimeout(this.finalizar.bind(this),10);
                         break;
                 }
 
             } else if (this.segundoEmoji === undefined) {
                 return;
             } else if (this.emojiReferencia != this.segundoEmoji) {
-
                 setTimeout(() => {
                     this.taparCelda(this.celda1, this.celda2);
                 }, 300);
-
                 this.primerEmoji = undefined;
                 this.segundoEmoji = undefined;
             }
         }
-
-/*        if (this.primerEmoji == this.segundoEmoji) {
-            if (this.celdaReferencia1 == this.celda1 || this.celdaReferencia2 == this.celda2) {
-                this.primerEmoji = undefined;
-                this.segundoEmoji = undefined;
-                this.celda1.removeEventListener('click', this.despejar);
-                this.celda2.removeEventListener('click', this.despejar);
-                this.intentos = 1;
-                this.puntos(this.intentos);
-            }
-        } else {
-
-        }
-
     }
-
-    puntos(intentos) {
-        this.primerEmoji = undefined;
-        this.segundoEmoji = undefined;
-        this.celda1.removeEventListener('click', this.despejar);
-        this.celda2.removeEventListener('click', this.despejar);
-
-        let nodoPuntuacion = document.getElementById("puntuacion");
-
-        switch (intentos) {
-            case intentos = 1:
-                nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 10;
-                setTimeout(this.finalizar.bind(this), 10);
-                break;
-            case intentos = 2:
-                nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 5;
-                setTimeout(this.finalizar.bind(this), 10);
-                break;
-            case intentos = 3:
-                nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 2.5;
-                setTimeout(this.finalizar.bind(this), 10);
-                break;
-            case intentos > 3:
-                nodoPuntuacion.innerHTML = this.puntuacion = this.puntuacion + 0;
-                setTimeout(this.finalizar.bind(this), 10);
-                break;
-        }*/
-    }
-
     taparCelda(celda1, celda2) {
         celda1.style.backgroundColor = "darkslategray";
         celda2.style.backgroundColor = "darkslategray";
-
         celda1.innerHTML = '';
         celda2.innerHTML = '';
     }
-
     cronometro() {
-
         let tiempoRef = Date.now();
         let acumulado = 0;
-
         this.timer = setInterval(() => {
             let tiempo = document.getElementById("tiempo");
             acumulado += Date.now() - tiempoRef;
@@ -299,7 +203,6 @@ class JuegoMemoria extends Tablero {
             tiempo.innerHTML = this.formatearMS(acumulado);
         }, 1000 / 60)
     }
-
     formatearMS(tiempo_ms) {
         let MS = tiempo_ms % 1000;
         let S = Math.floor(((tiempo_ms - MS) / 1000) % 60);
@@ -310,25 +213,19 @@ class JuegoMemoria extends Tablero {
         }
         return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2) + "." + MS.ceros(3);
     }
-
     finalizar() {
         if (this.maxParejas == this.numParejas) {
-
             let tiempo = document.getElementById("tiempo").textContent;
             alert
                 (`
                     Enhorabuena!! El juego ha finalizado. 
-
                     Has tenido una puntuación de ${this.puntuacion} puntos.
         
                     Tu tiempo trancurrido ha sido de ${tiempo}`);
-
             window.clearInterval(this.timer);
         }
     }
-
 }
-
 window.onload = function () {
     // Iniciamos el juego.
     let juegoMemoria = new JuegoMemoria();
